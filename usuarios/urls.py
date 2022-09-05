c8ab2c6051d86a2f01f73c1880c494be37056635
Jeniffer-De <vivi-2222@hotmail.com>
2022-09-04 19:47:50 -0500

    login-registro

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from usuarios.views import login_request, registro 
 
 urlpatterns = [
-    
+   path("login/", login_request, name="login"), 
+   path("registro/", registro, name= "registro"),
+   
 ]
